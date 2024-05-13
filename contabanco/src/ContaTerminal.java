c75f5ac12b32561b01636cffd20cf2ccd2447378
LanCaroline <lana.de.sa@hotmail.com>
2024-05-13 14:17:16 -0300

    tratamento de entrada numérica

--- contabanco/src/ContaTerminal.java
+++ contabanco/src/ContaTerminal.java
@@ -17,14 +17,29 @@ public class ContaTerminal {
     public int getConta(){
         return conta;
     }
+    public void setConta(int conta){
+        this.conta = conta;
+    }
     public String getAgencia(){
         return agencia;
+    }
+
+    public void setAgencia(String agencia){
+        this.agencia = agencia;
     }
     public String getNomeCliente(){
         return nomeCliente;
     }
+
+    public void setNomeCliente(String nomeCliente){
+        this.nomeCliente = nomeCliente;
+    }
     public double getSaldo() {
         return saldo;
+    }
+
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
     }
 
 }
--- contabanco/src/Usuario.java
+++ contabanco/src/Usuario.java
@@ -2,10 +2,51 @@ import java.util.Scanner;
 
 public class Usuario {
 
+    /*
+     * Uso do Scanner
+     *
+     * String - nextLint();
+     * Int - nextInt();
+     * Float - nextFloat();
+     * Double - nextDouble();
+     * Boolean - nextBoolean();
+     */
+
     public static void main(String[] args) {
 
-        dadosParaCadastro();
         ContaTerminal contaTerminal = new ContaTerminal();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da conta: ");
+        while(scan.hasNext()){
+            if(scan.hasNextInt()){
+                int conta = scan.nextInt();
+                contaTerminal.setConta(conta);
+                break;
+            }else{
+                System.out.println("Neste campo, digite apenas números.");
+                scan.nextLine();
+            }
+        }
+
+        System.out.println("Por favor, digite o número da agência: ");
+        String agencia = scan.nextLine();
+        contaTerminal.setAgencia(agencia);
+
+        System.out.println("Por favor, digite seu nome: ");
+        String nome = scan.nextLine();
+        contaTerminal.setNomeCliente(nome);
+
+        System.out.println("Por favor, digite o valor de seu saldo: ");
+        while(scan.hasNext()){
+            if(scan.hasNextInt()){
+                double saldo = scan.nextInt();
+                contaTerminal.setSaldo(saldo);
+                break;
+            }else{
+                System.out.println("Neste campo, digite apenas números.");
+                scan.nextLine();
+            }
+        }
 
         System.out.println("Olá "+contaTerminal.getNomeCliente()+
                 ", obrigado por criar uma conta em nosso banco, sua agência é "+contaTerminal.getAgencia()+
@@ -13,58 +54,6 @@ public class Usuario {
                 ", e seu saldo "+contaTerminal.getSaldo()+
                 " Já está disponível para saque.");
 
-    }
-
-    public static void dadosParaCadastro(){
-
-        int conta = 0;
-        String agencia = "";
-        String nomeCliente = "";
-        double saldo = 0.0;
-
-        System.out.println("================== Bem-Vindo ao seu Banco ==================");
-
-        int i = 5;
-        while(i!=0){
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.println("Menu: \n"+
-                    "[1] - Número da conta. \n"+
-                    "[2] - Número da agência. \n"+
-                    "[3] - Nome Completo. \n"+
-                    "[4] - Valor do saldo. \n"+
-                    "[0] - Sair. \n"+
-                    "================== Bem-Vindo ao seu Banco ==================");
-
-            if(scanner.toString().equals("1")){
-                System.out.println("Digite o número da conta: ");
-                String numero = scanner.toString();
-                conta = Integer.parseInt(numero);
-                i++;
-            }
-            if (scanner.toString().equals("2")){
-                System.out.println("Digite o número da agência: ");
-                agencia = scanner.toString();
-                i++;
-            }
-            if (scanner.toString().equals("3")){
-                System.out.println("Digite seu nome completo: ");
-                nomeCliente = scanner.toString();
-                i++;
-            }
-            if (scanner.toString().equals("4")){
-                System.out.println("Digite o valor do saldo: ");
-                String numero = scanner.toString();
-                saldo = Double.parseDouble(numero);
-                i++;
-            }
-
-            ContaTerminal contaTerminal = new ContaTerminal(conta, agencia, nomeCliente,saldo);
-
-            if (scanner.toString().equals("0")){
-                i = 0;
-            }
-        }
     }
 
 }
