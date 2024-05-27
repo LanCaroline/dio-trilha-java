6a2572fa1d435794e228d16543910ec411d9ebf5
LanCaroline <lana.de.sa@hotmail.com>
2024-05-27 17:35:39 -0300

    introdução a collections e exercício lista de tarefas

---
+++ Collections/src/main/list/operacoesBasicas/ListaTarefas.java
@@ -0,0 +1,48 @@
+package main.list.operacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+
+    private List<Tarefa> listaTarefas;
+
+    public ListaTarefas(){
+        this.listaTarefas = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        listaTarefas.add(new Tarefa(descricao));
+
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasRemovidas = new ArrayList<>();
+        for(Tarefa tarefa: listaTarefas){
+            if(tarefa.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasRemovidas.add(tarefa);
+            }
+        }
+
+        listaTarefas.removeAll(tarefasRemovidas);
+    }
+
+    public int obterNumeroTotalTarefas(){
+        return listaTarefas.size();
+    }
+
+    public void obterDescricoesTarefas() {
+
+        if (listaTarefas.isEmpty()) {
+            System.out.println("Lista de descrições vazia.");
+        }else {
+            for (Tarefa tarefa : listaTarefas) {
+                System.out.println(tarefa.getDescricao());
+            }
+              //System.out.println(listaTarefas);
+
+        }
+    }
+
+
+}
---
+++ Collections/src/main/list/operacoesBasicas/operacoesBasicasTeste/ListaTarefasTeste.java
@@ -0,0 +1,38 @@
+package main.list.operacoesBasicas.operacoesBasicasTeste;
+
+import main.list.operacoesBasicas.ListaTarefas;
+import main.list.operacoesBasicas.Tarefa;
+
+public class ListaTarefasTeste {
+
+    public static void main(String[] args) {
+        ListaTarefas listaTarefa = new ListaTarefas();
+
+        //obter número total de tarefas (o valor deve ser 0)
+        System.out.println("Total de elementos na lista: "+listaTarefa.obterNumeroTotalTarefas());
+
+        //adicionar tarefa
+//        listaTarefa.adicionarTarefa("Arrumar o guarda-roupa");
+//        listaTarefa.adicionarTarefa("Passar roupa");
+//        listaTarefa.adicionarTarefa("Lavar a louça");
+//        listaTarefa.adicionarTarefa("Lavar roupa");
+//        listaTarefa.adicionarTarefa("Arrumar a cama");
+
+        //obter número total de tarefas
+        System.out.println("Total de elementos na lista: "+listaTarefa.obterNumeroTotalTarefas());
+
+        //listar as tarefas/descrições
+        listaTarefa.obterDescricoesTarefas();
+
+        //remover tarefa
+        listaTarefa.removerTarefa("Passar roupa");
+        listaTarefa.removerTarefa("lavar roupa");
+
+        //obter número total de tarefas
+        System.out.println("Total de elementos na lista: "+listaTarefa.obterNumeroTotalTarefas());
+
+        //listar as tarefas/descrições
+        listaTarefa.obterDescricoesTarefas();
+
+    }
+}
